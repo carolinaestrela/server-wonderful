f03f586f95a9666e7a90b9fe1e57faf7654419c0
Carolina Estrela <estrela.carolina@bol.com.br>
2019-05-28 22:08:30 -0300

    Criado o servidor

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const http = require('http')
+const utf8 = require('utf8')
+
+const servidor = http.createServer(function (request, response) {
+    if (request.url === '/') {
+        response.end('Hello wonderful world!')
+    } else if (request.url === '/comidas') {
+        if (request.method === 'GET') {
+            response.writeHead(200, {
+                "Content-Type": "text/html;charset=utf-8"
+            })
+            response.end("<h1>Respostão diferentona</h1>")
+    } else if (request.method === 'POST') {
+        response.writeHead(200, {
+            "Content-Type": "text/html;charset=utf-8"
+        })
+        response.end("<h1>Respostão diferentona</h1>")
+        }
+    }
+})
+servidor.listen(3000)
+console.log("servidor wonderful rodando na porta 3000")
