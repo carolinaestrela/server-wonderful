8528aebaf6db58123d69335ac86c8e015c7133f8
Carolina Estrela <estrela.carolina@bol.com.br>
2019-05-29 22:00:56 -0300

    Correção servidor

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
+
 const http = require('http')
-const utf8 = require('utf8')
+const comidas = {
+    pratosFavoritos:
+    [
+    {
+      nome: "Batata frita",
+      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+      imagem: "img/Batata-frita.jpg"
+    },
+    {
+      nome: "Macarronada",
+      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+      imagem: "img/macarronada.jpg"
+    },
+    {
+      nome: "Falafel",
+      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+      imagem: "img/falafel.jpg"
+    },
+    {
+      nome: "Creme de abóbora",
+      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+      imagem: "img/creme-de-abobora.jpg"
+    },
+  
+  ]}
 
 const servidor = http.createServer(function (request, response) {
     if (request.url === '/') {
@@ -7,9 +32,11 @@ const servidor = http.createServer(function (request, response) {
     } else if (request.url === '/comidas') {
         if (request.method === 'GET') {
             response.writeHead(200, {
-                "Content-Type": "text/html;charset=utf-8"
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin':"*"
             })
-            response.end("<h1>Respostão diferentona</h1>")
+            response.write(JSON.stringify(comidas))
+            response.end()
     } else if (request.method === 'POST') {
         response.writeHead(200, {
             "Content-Type": "text/html;charset=utf-8"
@@ -19,4 +46,4 @@ const servidor = http.createServer(function (request, response) {
     }
 })
 servidor.listen(3000)
-console.log("servidor wonderful rodando na porta 3000")
+console.log("servidor wonderful rodando na porta 3000") 
